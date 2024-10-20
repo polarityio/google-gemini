9c5805433f09654516a247dfe80fba01fc15d1ce
Ed <ed@polarity.io>
2024-10-20 09:05:42 -0400

    INT-1347: Disable safety settings for cyber related questions

--- integration.js
+++ integration.js
@@ -167,7 +167,25 @@ async function askQuestion(messages, options) {
       generationConfig: {
         temperature: 0.1,
         candidateCount: 1
-      }
+      },
+      safetySettings: [
+        {
+          category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+          threshold: 'BLOCK_MEDIUM_AND_ABOVE'
+        },
+        {
+          category: 'HARM_CATEGORY_HATE_SPEECH',
+          threshold: 'BLOCK_MEDIUM_AND_ABOVE'
+        },
+        {
+          category: 'HARM_CATEGORY_HARASSMENT',
+          threshold: 'BLOCK_MEDIUM_AND_ABOVE'
+        },
+        {
+          category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+          threshold: 'BLOCK_NONE'
+        }
+      ]
     },
     method: 'POST',
     json: true
